beca4d5bd66a253d4d55e1030e8e5557a3d19052
Rohan Gupta <44989017+rohang9000@users.noreply.github.com>
2025-08-06 08:02:04 -0700

    Add files via upload

---
+++ cdk-hello-world-template/typescript/bin/hello-cdk.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { HelloCdkStack } from '../lib/hello-cdk-stack';
+
+const app = new cdk.App();
+new HelloCdkStack(app, 'HelloCdkStack', {
+  env: { account: '123456789012', region: 'us-east-1' },
+});
